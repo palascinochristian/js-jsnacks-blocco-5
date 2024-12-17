c602fac42e1f03864fe4d0056dac27a1c5678013
palascinochristian <palascinochristian@gmail.com>
2024-12-17 12:18:14 +0100

    Snack 4 -> Done

--- ex-js-array-methods-main/snack-4/script.js
+++ ex-js-array-methods-main/snack-4/script.js
@@ -7,3 +7,6 @@ const posts = [
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
+
+const authors = posts.map(elm => elm.author)
+console.log(authors)
