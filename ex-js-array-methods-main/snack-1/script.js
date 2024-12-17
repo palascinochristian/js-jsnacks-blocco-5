b74d6a8992bcfb3dfdbf582d315f308406269207
palascinochristian <palascinochristian@gmail.com>
2024-12-17 12:09:39 +0100

    Snack 1 -> Done

--- ex-js-array-methods-main/snack-1/script.js
+++ ex-js-array-methods-main/snack-1/script.js
@@ -2,3 +2,10 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+
+names.forEach((elm) => {
+    console.log(elm)
+})
+    
+    
